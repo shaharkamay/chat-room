bb9cf68b0112654bcaef5627dc0acf4a518ace82
shaharKamay <shahar.kamay@gmail.com>
2021-12-06 09:48:05 +0200

    added send icon to chat

--- client/src/components/chat/SendMessage.jsx
+++ client/src/components/chat/SendMessage.jsx
@@ -23,14 +23,18 @@ function SendMessage() {
     return (
         <div className="send-message">
             <form className="send-message__form" onSubmit={handleSubmit} >
-            <input 
-                type="text" 
-                placeholder="Type a message"
-                value={sendMessage}
-                onChange={e => setSendMessage(e.target.value)} 
-                className="send-message__input"
-            />
-            <button className="send-message__button">Send</button>
+                <input 
+                    type="text" 
+                    placeholder="Type a message"
+                    value={sendMessage}
+                    onChange={e => setSendMessage(e.target.value)} 
+                    className="send-message__input"
+                />
+                <button className="send-message__button">
+                    <svg className="send-svg">
+                        <path fill="currentColor" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path>
+                    </svg>
+                </button>
             </form>
         </div>
     )
