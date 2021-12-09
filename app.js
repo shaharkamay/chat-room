cbbcc9bfdfcb7d4d2f7f32f5a55c9df6cfbf4c58
shaharKamay <shahar.kamay@gmail.com>
2021-12-09 17:14:20 +0200

    refactory to app.js server-side

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import morgan from 'morgan';
 import morganHandler from './middleware/morgan.js';
 import errorHandler from './error-handling/error-handler.js';
 import apiRouter from './routes/api.js';
-import cookieParser from 'cookie-parser';
 import { render } from './controllers/app-controller.js';
 
 const app = express();
@@ -12,17 +11,13 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser());
 app.use(
   morganHandler,
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
 app.use(express.static("./build"));
-app.get("/", (req, res) => {
-  console.log("llllllll")
-  res.sendFile("./index.html");
-});
+app.get("/", render);
 
 app.get("/login", render);
 app.get("/sign-up", render);
